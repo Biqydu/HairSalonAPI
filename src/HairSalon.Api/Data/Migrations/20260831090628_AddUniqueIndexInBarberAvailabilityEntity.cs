d684b140f55516e6644454da94c740b9707d8883
Biqydu <brajanpalgan5@gmail.com>
2026-08-31 11:07:25 +0200

    feat: add migration for unique constraint in BarberAvailability

---
+++ src/HairSalon.Api/Data/Migrations/20260831090628_AddUniqueIndexInBarberAvailabilityEntity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HairSalon.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexInBarberAvailabilityEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BarberAvailabilities_BarberId",
+                table: "BarberAvailabilities");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "BarberAvailabilities",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "BarberAvailabilities",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BarberAvailabilities_BarberId_DayOfWeek",
+                table: "BarberAvailabilities",
+                columns: new[] { "BarberId", "DayOfWeek" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BarberAvailabilities_BarberId_DayOfWeek",
+                table: "BarberAvailabilities");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "BarberAvailabilities");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "BarberAvailabilities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BarberAvailabilities_BarberId",
+                table: "BarberAvailabilities",
+                column: "BarberId");
+        }
+    }
+}
